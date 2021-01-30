a536548ae633f6db76581b18808026b81ef2f64d
vishalwix <vishalwix5@gmail.com>
2021-01-30 12:59:29 +0530

    First Change

---
+++ SpringBootJwtAuthenticationExamples/src/main/java/com/mindbowser/assignment/model/Manager.java
@@ -0,0 +1,171 @@
+package com.mindbowser.assignment.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name = "manager", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+            "username"
+        }),
+        @UniqueConstraint(columnNames = {
+            "email"
+        })
+})
+public class Manager{
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String first_name; 
+    
+
+	@NotBlank
+    @Size(min=6, max = 50)
+    private String last_name;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String username;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min=6, max = 100)
+    private String password;  
+    
+    
+    @NotBlank
+    @Size(min=6, max = 50)
+    private String dob;
+    
+    @NotBlank
+    @Size(min=6, max = 50)
+    private String company;
+    
+    @NotBlank
+    @Size(min=6, max = 50)
+    private String address;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", 
+    	joinColumns = @JoinColumn(name = "user_id"), 
+    	inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public Manager() {}
+
+    public Manager(String first_name, String last_name, String dob, String username, String email, String password, String company, String address) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.dob = dob;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.company = company;
+        this.address = address;         
+    }
+    
+    public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String fisrt_name) {
+		this.first_name = fisrt_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
